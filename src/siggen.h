08aa58b9b1df95cf4dacffbfe6e08d1117d4c262
Nick Torosian <goodvibrations32@protonmail.com>
2024-09-19 15:10:45 +0300

    feat(signal generation): Adding the bear minimum.      Using the standard oscillator stuff to create the basic wave-forms.      The supported waves now are:      **square, triangle saw dropping and saw upping**

---
+++ src/siggen.h
@@ -0,0 +1,8 @@
+#ifdef INC_SIGGEN_H_
+
+double sqtick(oscb_t *osc, double freq);
+double sawdtick(oscb_t *osc, double freq);
+double sawutick(oscb_t *osc, double freq);
+double tringtick(oscb_t *osc, double freq);
+
+#endif
