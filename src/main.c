ccd40aafb3695ff8826369d5d5e911834ccac6c8
Nick Torosian <goodvibrations32@protonmail.com>
2024-10-06 16:11:56 +0300

    make ignoring nicer and save to make a break

--- src/main.c
+++ src/main.c
@@ -1,16 +1,20 @@
 /*******************************************************************************
-** envx.c :
-*           extract amplitude envelope from mono soundfile
+ ** main.c :
+*          the entry point of all
 *******************************************************************************/
 #include <math.h>
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <portsf.h>
 #include <time.h>
 
+#include "siggen.h"
 #include "oscB.h"
 #define DEBUG         -DDEBUG
+/* #define MEMORY__DEBUG -DDEBUG */
 #define TBLLEN        (1024)
+#define DEFSFR        (44100)
 
 #define DEFAULT_WINDOW_MSECS        (0xf)
 
@@ -20,6 +24,7 @@
 int main(int argc, char *argv[]) {
   int error = 0;
   int nsamps;
+  unsigned long nosc;
 
   /* if (argc < arg_nargs){ */
 
@@ -29,17 +34,21 @@ int main(int argc, char *argv[]) {
   /*   fprintf(stderr, "Error: Not enough arguments.\nYou gave %d instead of %d\n", argc, arg_nargs - 1); */
   /*   #endif */
   /* } */
+
   // NOTE:: make this fuzzable
   fprintf(stderr, "Give a number for samples: \n");
   scanf("%d", &nsamps);
 
+  // NOTE:: make this fuzzable
+  /* fprintf(stderr, "Give a number for oscilators in the bank: \n"); */
+  /* scanf("%ld", &nosc); */
+
+  nosc = nsamps;
+
   // NOTE:: make an oscilator bank......
   oscb_t **osc = NULL;
   double *osc_freqs = NULL, *osc_amps = NULL;
-  unsigned long nosc;
-  double outframe[nsamps];
-
-  nosc = nsamps;
+  /* double outframe[nsamps]; */
 
   osc_amps = (double *) malloc(sizeof(double) * nosc);
   if (osc_amps == NULL) {
@@ -65,47 +74,67 @@ int main(int argc, char *argv[]) {
   // NOTE::
   //        Initializing all the oscillators
   for (int i = 0; i < nosc; i++) {
-    osc[i] = new_oscil(44100);
+    osc[i] = new_oscil(DEFSFR);
     if (osc[i] == NULL) {
       puts("No memory for oscillators\n");
       error++;
       goto exit;
     }
+    #ifdef MEMORY__DEBUG
+    /* fprintf(stderr, "%d\t%lf\n", i, *osc[i]); */
+    fprintf(stderr, "%pt\n", osc[i]);
+    /* assert(*osc[i]-> == 0.0); */
+    #endif
   }
 
   clock_t startt, endt;
 
   startt = clock();
 
-  // NOTE::
-  //        making a lookup table for a sine wave of 1024 length
-  unsigned long i, len = TBLLEN;
-  double step, table[TBLLEN];
-  step = twopi / len;
-  // NOTE::
-  //        the array has one cycle because TBLLEN == twopi
-  for (i = 0; i < TBLLEN; i++) {
-    table[i] = sin(step * i);
+  // TODO:: make the signal to be printed in seconds instead of the idx array.
+  //        here im able to determine the ammount of seconds a signal has
+  //        because i have the sampling frequency and the number of samples the
+  //        user gave... this is bad idea i have to determine the exact amount
+  //        of samples via the Niquist theorem of sf and aliasing... I see a lot
+  //        now...
+  double dur = (double) nosc / (double) DEFSFR;
+  fprintf(stderr, "duration in seconds: %lf\n", dur);
+  double sigbuf[DEFSFR];
+  for (int i = 0; i < nosc; i++){
+    /* double some =  sinetick(osc[i], 200.0); */
+    sigbuf[i] = sinetick(*osc, 200);
+    fprintf(stdout, "%d\t%lf\n", i, sigbuf[i]);
   }
-  /* fprintf(stdout, "%d\t%lf\n", TBLLEN, table[TBLLEN-1]); */
 
   // NOTE::
-  //        truncting loop
-  double curphase = 0.0;
-  double tablen = (double) TBLLEN;
-  double incr = step;
-  for (i = 0; i < nsamps; i++) {
-    int idx = (int) curphase; // phase truncated
-    outframe[i] = (float) table[idx];
-    curphase += incr;
-    while (curphase >= tablen) {
-      curphase -= tablen;
-    }
-    while (curphase < tablen) {
-      curphase += tablen;
-    }
-    printf("%lu\t%lf\n", i, table[i]);
-  }
+  //        making a lookup table for a sine wave of 1024 length
+  /* unsigned long i, len = TBLLEN; */
+  /* double step, table[TBLLEN]; */
+  /* step = twopi / len; */
+  /* // NOTE:: */
+  /* //        the array has one cycle because TBLLEN == twopi */
+  /* for (i = 0; i < TBLLEN; i++) { */
+  /*   table[i] = sin(step * i); */
+  /* } */
+  /* /\* fprintf(stdout, "%d\t%lf\n", TBLLEN, table[TBLLEN-1]); *\/ */
+
+  /* // NOTE:: */
+  /* //        truncting loop */
+  /* double curphase = 0.0; */
+  /* double tablen = (double) TBLLEN; */
+  /* double incr = step; */
+  /* for (i = 0; i < tablen; i++) { */
+  /*   int idx = (int) curphase; // phase truncated */
+  /*   outframe[i] = (float) table[idx]; */
+  /*   curphase += incr; */
+  /*   while (curphase >= tablen) { */
+  /*     curphase -= tablen; */
+  /*   } */
+  /*   while (curphase < tablen) { */
+  /*     curphase += tablen; */
+  /*   } */
+  /*   printf("%lu\t%lf\n", i, table[i]); */
+  /* } */
 
   exit:
   if (osc_amps)
@@ -122,5 +151,5 @@ int main(int argc, char *argv[]) {
 
   endt = clock();
   double elapsed = (endt - startt) / (double) CLOCKS_PER_SEC;
-  fprintf(stderr, "Time to procces was: %ld [ms]\n", endt/100000);
+  fprintf(stderr, "Time to procces was: %f [ms]\n", elapsed);
 }
